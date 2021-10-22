60e75e1b8724e4f01fafa871dcef991cc03a1a3d
sbhusal123 <suryabhusal11@gmail.com>
2021-10-22 23:37:29 +0545

    Multidimensional array, pointer, call by ref.

--- basics/pointers/multi_dim_array_pointers/README.md
+++ basics/pointers/multi_dim_array_pointers/README.md
@@ -31,3 +31,57 @@
             *x[1]=4
             ------------------------------------------------
 ```
+
+**ii. Two Dimensional Array Pointer:**
+
+-   Size of array = 2 \* 3
+-   Can be thought of as a two slices with three sub slices in each.
+-   **Pointer Initialization:** `int(*p)[<size_of_sub_slice>]=&x`
+
+```text
+int x[2][3] = { {1, 2, 3}, {4, 5, 6} }
+int(*p)[3]=&x
+
+
+--------- Address of each element -------------
+&x[0][0]=-9648
+&x[0][1]=-9644
+&x[0][2]=-9640
+&x[1][0]=-9636
+&x[1][1]=-9632
+&x[1][2]=-9628
+------------------------------------------------
+
+--------- Address of &p[i][j] ----------------
+&p[0][0]=-9648
+&p[0][1]=-9644
+&p[0][2]=-9640
+&p[1][0]=-9636
+&p[1][1]=-9632
+&p[1][2]=-9628
+------------------------------------------------
+
+--------- Address of p[i]+j ----------------
+p[0]+j=0
+p[0]+j=1
+------------------------------------------------
+
+------------- Add &p[i] ----------------------
+&p[0]=-9648
+&p[1]=-9636
+------------------------------------------------
+```
+
+**iii. Passing Two Dimensional Array Pointer to function:**
+
+```text
+<!-- function signature -->
+void inplace_increament(int *arr, int size_x, int size_y, int inc_by)
+// each element can be dereferenced as *(&arr[i] + j);
+
+<!-- Array initialization -->
+int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+<!-- Passing to function -->
+inplace_increament(&arr, 2, 3, 2);
+```
--- basics/pointers/multi_dim_array_pointers/main.c
+++ basics/pointers/multi_dim_array_pointers/main.c
@@ -72,8 +72,140 @@ void two_d_array_deref_address()
     */
 }
 
+void two_d_array_pointer()
+{
+    int x[2][3] = {
+        {1, 2, 3}, {4, 5, 6}};
+
+    int(*p)[3] = &x;
+
+    printf("int x[2][3] = { {1, 2, 3}, {4, 5, 6} }\n");
+    printf("int(*p)[3]=&x\n");
+    printf("\n\n");
+
+    printf("--------- Address of each element -------------\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("&x[%d][%d]=%d\n", i, j, &x[i][j]);
+        }
+    }
+    printf("------------------------------------------------");
+
+    printf("\n\n");
+
+    printf("--------- Address of &p[i][j] ----------------\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("&p[%d][%d]=%d\n", i, j, &p[i][j]);
+        }
+    }
+    printf("------------------------------------------------");
+    printf("\n\n");
+
+    printf("--------- Address of p[i]+j ----------------\n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("p[0]+j=%d\n", i, p[0] + i);
+    }
+    printf("------------------------------------------------");
+    printf("\n\n");
+
+    printf("------------- Add &p[i] ----------------------\n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("&p[%d]=%d \n", i, &p[i]);
+    }
+    printf("------------------------------------------------");
+
+    printf("\n\n");
+
+    /*
+    Output:
+
+        int x[2][3] = { {1, 2, 3}, {4, 5, 6} }
+        int(*p)[3]=&x
+
+
+        --------- Address of each element -------------
+        &x[0][0]=-9648
+        &x[0][1]=-9644
+        &x[0][2]=-9640
+        &x[1][0]=-9636
+        &x[1][1]=-9632
+        &x[1][2]=-9628
+        ------------------------------------------------
+
+        --------- Address of &p[i][j] ----------------
+        &p[0][0]=-9648
+        &p[0][1]=-9644
+        &p[0][2]=-9640
+        &p[1][0]=-9636
+        &p[1][1]=-9632
+        &p[1][2]=-9628
+        ------------------------------------------------
+
+        --------- Address of p[i]+j ----------------
+        p[0]+j=0
+        p[0]+j=1
+        ------------------------------------------------
+
+        ------------- Add &p[i] ----------------------
+        &p[0]=-9648
+        &p[1]=-9636
+        ------------------------------------------------
+    */
+}
+
+void inplace_increament(int *arr, int size_x, int size_y, int inc_by)
+{
+    // Inplace increament at memory location
+    for (int i = 0; i < size_x; i++)
+        for (int j = 0; j < size_y; j++)
+            *(&arr[i] + j) += inc_by;
+}
+
 int main(int *argc, char *argv[])
 {
-    two_d_array_deref_address();
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+    printf("\n");
+    printf("Before increament\n");
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+            printf("arr[%d][%d]=%d \n", i, j, arr[i][j]);
+
+    // Inplace increament by
+    inplace_increament(&arr, 2, 3, 2);
+
+    printf("\n\n");
+    printf("After increament\n");
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 3; j++)
+            printf("arr[%d][%d]=%d \n", i, j, arr[i][j]);
+
+    printf("\n");
+
+    /*
+        Before increament
+        arr[0][0]=1 
+        arr[0][1]=2 
+        arr[0][2]=3 
+        arr[1][0]=4 
+        arr[1][1]=5 
+        arr[1][2]=6 
+
+
+        After increament
+        arr[0][0]=3 
+        arr[0][1]=6 
+        arr[0][2]=7 
+        arr[1][0]=6 
+        arr[1][1]=5 
+        arr[1][2]=6 
+    */
     return 0;
 }
